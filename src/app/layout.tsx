b0f6bfa4b4c73476789ef29766e769111a5a0c3c
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-29 13:50:51 +0600

    Update Providers path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Providers from "./lib/Providers/Providers";
+import Providers from "@/lib/Providers/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
