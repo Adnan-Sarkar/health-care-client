a1b90f4f9e5818eaabf184e7d9120a9af23ed0e4
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-29 12:55:48 +0600

    Update footer component

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
+import facebookIcon from "@/assets/landing_page/facebook.png";
 
 const Footer = () => {
   return (
@@ -25,6 +27,50 @@ const Footer = () => {
           </Typography>
           <Typography color={"#fff"} component={Link} href="/">
             NGOs
+          </Typography>
+        </Stack>
+
+        <Stack
+          direction={"row"}
+          gap={4}
+          alignItems={"center"}
+          justifyContent={"center"}
+          py={3}
+        >
+          <Image src={facebookIcon} alt="facebook" width={30} height={30} />
+          <Image src={facebookIcon} alt="facebook" width={30} height={30} />
+          <Image src={facebookIcon} alt="facebook" width={30} height={30} />
+          <Image src={facebookIcon} alt="facebook" width={30} height={30} />
+        </Stack>
+        <div className="border-b-[1px] border-dashed my-5"></div>
+        <Stack
+          direction={"row"}
+          gap={2}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          py={3}
+        >
+          <Typography component="p" color={"#fff"}>
+            &copy;2024 Health Care. All Rights Reserved.
+          </Typography>
+          <Typography
+            color={"#fff"}
+            component={Link}
+            href="/"
+            variant="h4"
+            fontWeight={600}
+          >
+            <Box component={"span"} color={"primary.main"}>
+              H
+            </Box>
+            ealth{" "}
+            <Box component={"span"} color={"primary.main"}>
+              C
+            </Box>
+            are
+          </Typography>
+          <Typography component="p" color={"#fff"}>
+            Privacy Policy | Terms & Conditions
           </Typography>
         </Stack>
       </Container>
