30f869a0267005b7b251393fd5d9d19a375e2df1
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-04-30 16:48:17 +0600

    Create Doctor type

---
+++ src/types/index.types.ts
@@ -0,0 +1,21 @@
+export type TDoctor = {
+  id: string;
+  email: string;
+  name: string;
+  profilePhoto: string;
+  contactNumber: string;
+  address: string;
+  registrationNumber: string;
+  experience: number;
+  gender: "MALE" | "FEMALE";
+  apointmentFee: number;
+  qualification: string;
+  currentWorkingPlace: string;
+  designation: string;
+  isDeleted: false;
+  createdAt: string;
+  updatedAt: string;
+  averageRating: number;
+  review: [];
+  doctorSpecialties: [];
+};
