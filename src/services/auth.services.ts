77365ceca82b7cd9689b444b7c0114f3700c92a3
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-02 15:01:39 +0600

    Create auth services for getUserInfo, removeUserInfo, isLoggedIn

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,6 +1,37 @@
-import { setToLocalStorage } from "@/utils/local-storage";
+import {
+  getFromLocalStoreage,
+  removeFromLocalStorage,
+  setToLocalStorage,
+} from "@/utils/local-storage";
 import { authKey } from "@/constants/authKey";
+import { decodedToken } from "@/utils/jwt";
 
 export const storeUserInfo = (token: string) => {
   return setToLocalStorage(authKey, token);
+};
+
+export const getUserInfo = () => {
+  const authToken = getFromLocalStoreage(authKey);
+
+  if (authToken) {
+    const decodedData: any = decodedToken(authToken);
+
+    return {
+      ...decodedData,
+      role: decodedData?.role.toLowerCase(),
+    };
+  }
+};
+
+export const removeUserInfo = () => {
+  return removeFromLocalStorage(authKey);
+};
+
+export const isLoggedIn = () => {
+  const authToken = getFromLocalStoreage(authKey);
+  if (authToken) {
+    return true;
+  }
+
+  return false;
 };
