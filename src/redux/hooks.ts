5def092d32c477eb6eae773b4de28679dfb1c9a2
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-09 21:53:46 +0600

    Create new custom hook debounce

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,5 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
+import { useEffect, useState } from "react";
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+type TDebounceProps = {
+  searchQuery: string;
+  delay: number;
+};
+export const useDebounced = ({ searchQuery, delay }: TDebounceProps) => {
+  const [debouncedValue, setDebouncedValue] = useState<string>(searchQuery);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(searchQuery);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [searchQuery, delay]);
+
+  return debouncedValue;
+};
