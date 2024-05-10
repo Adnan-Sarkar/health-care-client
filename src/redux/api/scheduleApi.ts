8f410f73016bf604f2700bd373aeca61032e33ed
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-10 11:22:28 +0600

    Create scheduleApi endpoint using rtk-query

---
+++ src/redux/api/scheduleApi.ts
@@ -0,0 +1,45 @@
+import { TMeta } from "@/types/index.types";
+import { tagTypes } from "../tag-types";
+import { baseApi } from "./baseApi";
+
+const scheduleApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    createSchedule: builder.mutation({
+      query: (data) => ({
+        url: `/schedule`,
+        method: "POST",
+        data,
+      }),
+      invalidatesTags: [tagTypes.schedule],
+    }),
+
+    getAllSchedules: builder.query({
+      query: (args: Record<string, any>) => ({
+        url: `/schedule`,
+        method: "GET",
+        params: args,
+      }),
+      transformResponse: (response: [], meta: TMeta) => {
+        return {
+          doctors: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.schedule],
+    }),
+
+    deleteSchedule: builder.mutation({
+      query: (id) => ({
+        url: `/schedule/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.schedule],
+    }),
+  }),
+});
+
+export const {
+  useCreateScheduleMutation,
+  useGetAllSchedulesQuery,
+  useDeleteScheduleMutation,
+} = scheduleApi;
