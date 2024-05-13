a2cc945f71cb98ee75229fc85b2c8b0163f4b42f
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-14 00:31:16 +0600

    Update doctorScheduleApi with adding more api endpoint to get schedules

--- src/redux/api/doctorScheduleApi.ts
+++ src/redux/api/doctorScheduleApi.ts
@@ -1,5 +1,6 @@
 import {baseApi} from "@/redux/api/baseApi";
 import {tagTypes} from "@/redux/tag-types";
+import {TMeta} from "@/types/index.types";
 
 export const doctorScheduleApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
@@ -11,7 +12,45 @@ export const doctorScheduleApi = baseApi.injectEndpoints({
             }),
             invalidatesTags: [tagTypes.doctorSchedule],
         }),
+
+        getAllDoctorSchedules: builder.query({
+            query: (arg: Record<string, any>) => {
+                return {
+                    url: '/doctor-schedule',
+                    method: 'GET',
+                    params: arg,
+                };
+            },
+            transformResponse: (response: [], meta: TMeta) => {
+                return {
+                    doctorSchedules: response,
+                    meta,
+                };
+            },
+            providesTags: [tagTypes.doctorSchedule],
+        }),
+
+        getDoctorSchedule: builder.query({
+            query: (id: string | string[] | undefined) => ({
+                url: `/doctor-schedule/${id}`,
+                method: 'GET',
+            }),
+            providesTags: [tagTypes.doctorSchedule],
+        }),
+
+        getMySchedule: builder.query({
+            query: () => ({
+                url: '/doctor-schedule/my-schedules',
+                method: 'GET',
+            }),
+            providesTags: [tagTypes.doctorSchedule],
+        }),
     }),
 });
 
-export const {useCreateDoctorScheduleMutation} = doctorScheduleApi
+export const {
+    useCreateDoctorScheduleMutation,
+    useGetAllDoctorSchedulesQuery,
+    useGetDoctorScheduleQuery,
+    useGetMyScheduleQuery
+} = doctorScheduleApi
