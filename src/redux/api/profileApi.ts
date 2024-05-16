753e0ab5cf0151701dd23fc6ad8d274cfcdc92f3
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-16 20:50:54 +0600

    Create profileApi to get and update user profile information using rtk-query

---
+++ src/redux/api/profileApi.ts
@@ -0,0 +1,29 @@
+import {tagTypes} from "../tag-types";
+import {baseApi} from "./baseApi";
+
+const profileApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getMyProfile: builder.query({
+            query: () => ({
+                url: `/user/me`,
+                method: "GET",
+            }),
+            providesTags: [tagTypes.user],
+        }),
+
+        updateMyProfile: builder.mutation({
+            query: (data) => ({
+                url: `/user/update-my-profile`,
+                method: "PATCH",
+                contentType: 'multipart/form-data',
+                data,
+            }),
+            invalidatesTags: [tagTypes.user],
+        }),
+    }),
+});
+
+export const {
+    useGetMyProfileQuery,
+    useUpdateMyProfileMutation,
+} = profileApi;
