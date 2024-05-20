6622ef5718787b85ffcdb65e73bc07c8b6a0e140
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-20 22:41:30 +0600

    Update doctorApi in rtk-query

--- src/redux/api/doctorApi.ts
+++ src/redux/api/doctorApi.ts
@@ -1,65 +1,65 @@
-import { TDoctor, TMeta } from "@/types/index.types";
-import { tagTypes } from "../tag-types";
-import { baseApi } from "./baseApi";
+import {TDoctor, TMeta} from "@/types/index.types";
+import {tagTypes} from "../tag-types";
+import {baseApi} from "./baseApi";
 
 const doctorApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({
-    createDoctor: builder.mutation({
-      query: (data) => ({
-        url: `/user/create-doctor`,
-        method: "POST",
-        contentType: "multipart/Form-data",
-        data,
-      }),
-      invalidatesTags: [tagTypes.doctor],
-    }),
+    endpoints: (builder) => ({
+        createDoctor: builder.mutation({
+            query: (data) => ({
+                url: `/user/create-doctor`,
+                method: "POST",
+                contentType: "multipart/Form-data",
+                data,
+            }),
+            invalidatesTags: [tagTypes.doctor],
+        }),
 
-    getAllDoctors: builder.query({
-      query: (args: Record<string, any>) => ({
-        url: `/doctor`,
-        method: "GET",
-        params: args,
-      }),
-      transformResponse: (response: TDoctor[], meta: TMeta) => {
-        return {
-          doctors: response,
-          meta,
-        };
-      },
-      providesTags: [tagTypes.doctor],
-    }),
+        getAllDoctors: builder.query({
+            query: (args: Record<string, any>) => ({
+                url: `/doctor`,
+                method: "GET",
+                params: args,
+            }),
+            transformResponse: (response: TDoctor[], meta: TMeta) => {
+                return {
+                    doctors: response,
+                    meta,
+                };
+            },
+            providesTags: [tagTypes.doctor],
+        }),
 
-    getSingleDoctor: builder.query({
-      query: (id: string) => ({
-        url: `/doctor/${id}`,
-        method: "GET",
-      }),
-      providesTags: [tagTypes.doctor],
-    }),
+        getSingleDoctor: builder.query({
+            query: (id: string) => ({
+                url: `/doctor/${id}`,
+                method: "GET",
+            }),
+            providesTags: [tagTypes.doctor],
+        }),
 
-    updateDoctor: builder.mutation({
-      query: (data) => ({
-        url: `/doctor/${data.id}`,
-        method: "PATCH",
-        data: data.body,
-      }),
-      invalidatesTags: [tagTypes.doctor],
-    }),
+        updateDoctor: builder.mutation({
+            query: (data) => ({
+                url: `/doctor/${data.id}`,
+                method: "PATCH",
+                data: data.body,
+            }),
+            invalidatesTags: [tagTypes.doctor, tagTypes.user],
+        }),
 
-    deleteDoctor: builder.mutation({
-      query: (id) => ({
-        url: `/doctor/soft/${id}`,
-        method: "DELETE",
-      }),
-      invalidatesTags: [tagTypes.doctor],
+        deleteDoctor: builder.mutation({
+            query: (id) => ({
+                url: `/doctor/soft/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: [tagTypes.doctor],
+        }),
     }),
-  }),
 });
 
 export const {
-  useCreateDoctorMutation,
-  useGetAllDoctorsQuery,
-  useGetSingleDoctorQuery,
-  useUpdateDoctorMutation,
-  useDeleteDoctorMutation,
+    useCreateDoctorMutation,
+    useGetAllDoctorsQuery,
+    useGetSingleDoctorQuery,
+    useUpdateDoctorMutation,
+    useDeleteDoctorMutation,
 } = doctorApi;
