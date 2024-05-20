2b15e878538ccad5a70fe15fd3c10a088678cd5a
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-20 19:59:19 +0600

    Update input field with set default value for variant property

--- src/components/Forms/CustomInputField.tsx
+++ src/components/Forms/CustomInputField.tsx
@@ -1,54 +1,54 @@
-import { SxProps, TextField } from "@mui/material";
-import { Controller, useFormContext } from "react-hook-form";
+import {SxProps, TextField} from "@mui/material";
+import {Controller, useFormContext} from "react-hook-form";
 
 type CustomInputFieldProps = {
-  name: string;
-  label: string;
-  type?: string;
-  size?: "small" | "medium";
-  fullWidth?: boolean;
-  variant: "outlined" | "filled" | "standard";
-  placeholder?: string;
-  required?: boolean;
-  sx?: SxProps;
+    name: string;
+    label: string;
+    type?: string;
+    size?: "small" | "medium";
+    fullWidth?: boolean;
+    variant?: "outlined" | "filled" | "standard";
+    placeholder?: string;
+    required?: boolean;
+    sx?: SxProps;
 };
 
 const CustomInputField = ({
-  name,
-  label,
-  type,
-  size = "small",
-  fullWidth = false,
-  variant,
-  placeholder,
-  required,
-  sx,
-}: CustomInputFieldProps) => {
-  const { control } = useFormContext();
+                              name,
+                              label,
+                              type,
+                              size = "small",
+                              fullWidth = false,
+                              variant = "outlined",
+                              placeholder,
+                              required,
+                              sx,
+                          }: CustomInputFieldProps) => {
+    const {control} = useFormContext();
 
-  return (
-    <Controller
-      control={control}
-      name={name}
-      render={({ field, fieldState: { error } }) => {
-        return (
-          <TextField
-            variant={variant}
-            label={label}
-            type={type}
-            size={size}
-            fullWidth={fullWidth}
-            placeholder={placeholder}
-            required={required}
-            {...field}
-            sx={{ ...sx }}
-            error={!!error?.message}
-            helperText={error?.message}
-          />
-        );
-      }}
-    />
-  );
+    return (
+        <Controller
+            control={control}
+            name={name}
+            render={({field, fieldState: {error}}) => {
+                return (
+                    <TextField
+                        variant={variant}
+                        label={label}
+                        type={type}
+                        size={size}
+                        fullWidth={fullWidth}
+                        placeholder={placeholder}
+                        required={required}
+                        {...field}
+                        sx={{...sx}}
+                        error={!!error?.message}
+                        helperText={error?.message}
+                    />
+                );
+            }}
+        />
+    );
 };
 
 export default CustomInputField;
