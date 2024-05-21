644b13dff76d49227f626b969e62bdef42319fb8
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-21 23:14:59 +0600

    Update axios instance with setting accessToken into cookies

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -3,6 +3,7 @@ import {TGenericErrorResponse, TResponseSuccessType,} from "@/types/index.types"
 import {getFromLocalStoreage, setToLocalStorage} from "@/utils/local-storage";
 import axios from "axios";
 import {getNewAccessToken} from "@/services/auth.services";
+import setAccessToken from "@/services/actions/setAccessToken";
 
 const axiosInstance = axios.create();
 
@@ -12,50 +13,50 @@ axiosInstance.defaults.timeout = 60000;
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
-  function (config) {
-    const accessToken = getFromLocalStoreage(authKey);
-    if (accessToken) {
-      config.headers.Authorization = accessToken;
+    function (config) {
+        const accessToken = getFromLocalStoreage(authKey);
+        if (accessToken) {
+            config.headers.Authorization = accessToken;
+        }
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error);
     }
-    return config;
-  },
-  function (error) {
-    return Promise.reject(error);
-  }
 );
 
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
-  // @ts-ignore
-  function (response) {
-    const responseObj: TResponseSuccessType = {
-      data: response?.data?.data,
-      meta: response?.data?.meta,
-    };
+    // @ts-ignore
+    function (response) {
+        const responseObj: TResponseSuccessType = {
+            data: response?.data?.data,
+            meta: response?.data?.meta,
+        };
 
-    return responseObj;
-  },
-  async function (error) {
-      const config = error.config;
-      if (error?.response?.status === 500 && !config.sent) {
-        config.sent = true;
-        const response = await getNewAccessToken();
-        const accessToken = response?.data?.access_token
-          config.headers["Authorization"] = accessToken;
-          setToLocalStorage(authKey, accessToken);
+        return responseObj;
+    },
+    async function (error) {
+        const config = error.config;
+        if (error?.response?.status === 500 && !config.sent) {
+            config.sent = true;
+            const response = await getNewAccessToken();
+            const accessToken = response?.data?.access_token
+            config.headers["Authorization"] = accessToken;
+            setToLocalStorage(authKey, accessToken);
+            setAccessToken(accessToken);
+            
+            return axiosInstance(config);
+        } else {
+            const responseObj: TGenericErrorResponse = {
+                statusCode: error?.response?.data?.statusCode || 500,
+                message: error?.response?.data?.message || "Something went wrong!",
+                errorMessages: error?.response?.data?.message,
+            };
 
-          return axiosInstance(config);
-      }
-      else {
-          const responseObj: TGenericErrorResponse = {
-              statusCode: error?.response?.data?.statusCode || 500,
-              message: error?.response?.data?.message || "Something went wrong!",
-              errorMessages: error?.response?.data?.message,
-          };
-
-          return responseObj;
-      }
-  }
+            return responseObj;
+        }
+    }
 );
 
-export { axiosInstance };
+export {axiosInstance};
