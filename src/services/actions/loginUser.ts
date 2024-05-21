783146c956814078662d029a81d51f299281d38a
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-21 23:13:42 +0600

    Update loginUser server action set accessToken into cookies

--- src/services/actions/loginUser.ts
+++ src/services/actions/loginUser.ts
@@ -1,17 +1,24 @@
 import {FieldValues} from "react-hook-form";
+import setAccessToken from "@/services/actions/setAccessToken";
 
 export const loginUser = async (data: FieldValues) => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      credentials: "include"
-    }
-  );
+    const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/auth/login`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+            credentials: "include"
+        }
+    );
 
-  return await res.json();
+    const userInfo = await res.json();
+
+    setAccessToken(userInfo?.data?.accessToken, {
+        redirect: "/dashboard"
+    })
+
+    return userInfo;
 };
