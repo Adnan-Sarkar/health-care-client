23c18e847a87f0224e0327d4a571981d038b2524
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-21 23:16:20 +0600

    Create middleware to implement authentication and authorization

---
+++ src/middleware.ts
@@ -0,0 +1,49 @@
+import type {NextRequest} from 'next/server'
+import {NextResponse} from 'next/server'
+import {cookies} from "next/headers";
+import {authKey} from "@/constants/authKey";
+import {jwtDecode} from "jwt-decode";
+import {USER_ROLE} from "@/constants/role";
+
+const AuthRoutes = ['/login', '/register'];
+const commonPrivateRoutes = ['/dashboard', '/dashboard/change-password'];
+const roleBasedPrivateRoutes = {
+    PATIENT: [/^\/dashboard\/patient/],
+    DOCTOR: [/^\/dashboard\/doctor/],
+    ADMIN: [/^\/dashboard\/admin/],
+    SUPER_ADMIN: [/^\/dashboard\/super-admin/],
+};
+
+export function middleware(request: NextRequest) {
+    const {pathname} = request.nextUrl;
+    const accessToken = cookies().get(authKey)?.value;
+
+    if (!accessToken) {
+        if (AuthRoutes.includes(pathname)) {
+            return NextResponse.next();
+        } else {
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
+    }
+
+    if (accessToken && commonPrivateRoutes.includes(pathname)) {
+        return NextResponse.next();
+    }
+
+    const decodedData = jwtDecode(accessToken) as any;
+    const role = decodedData?.role;
+
+    const userPrivateRoutes = roleBasedPrivateRoutes[role as keyof typeof USER_ROLE];
+
+    if (role && userPrivateRoutes) {
+        if (userPrivateRoutes.some(route => pathname.match(route))) {
+            return NextResponse.next();
+        }
+    }
+
+    return NextResponse.redirect(new URL('/', request.url))
+}
+
+export const config = {
+    matcher: ['/login', '/register', '/dashboard/:page*'],
+}
