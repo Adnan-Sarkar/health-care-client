0b985ed9eb5d268e30192eda134eb480d927e785
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-05-21 23:27:31 +0600

    Update AuthButton component's logout functionality

--- src/components/ui/AuthButton/AuthButton.tsx
+++ src/components/ui/AuthButton/AuthButton.tsx
@@ -1,30 +1,30 @@
-import { getUserInfo, removeUserInfo } from "@/services/auth.services";
-import { Button } from "@mui/material";
+import {getUserInfo} from "@/services/auth.services";
+import {Button} from "@mui/material";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
+import logoutUser from "@/services/actions/logoutUser";
 
 const AuthButton = () => {
-  const userInfo = getUserInfo();
-  const router = useRouter();
+    const userInfo = getUserInfo();
+    const router = useRouter();
 
-  const handleLogout = () => {
-    removeUserInfo();
-    router.refresh();
-  };
+    const handleLogout = () => {
+        logoutUser(router);
+    };
 
-  return (
-    <>
-      {userInfo && userInfo.userId ? (
-        <Button color="error" variant="contained" onClick={handleLogout}>
-          Logout
-        </Button>
-      ) : (
-        <Button component={Link} href="/login">
-          Login
-        </Button>
-      )}
-    </>
-  );
+    return (
+        <>
+            {userInfo && userInfo.userId ? (
+                <Button color="error" variant="contained" onClick={handleLogout}>
+                    Logout
+                </Button>
+            ) : (
+                <Button component={Link} href="/login">
+                    Login
+                </Button>
+            )}
+        </>
+    );
 };
 
 export default AuthButton;
