2d4c3f82ad64f796eecbba40a6aff1083faa9717
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-14 18:38:57 +0600

    Update navbar to add dashboard based on login user

--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -1,54 +1,62 @@
 "use client";
 
-import { Box, Container, Stack, Typography } from "@mui/material";
+import {Box, Container, Stack, Typography} from "@mui/material";
 import dynamic from "next/dynamic";
 import Link from "next/link";
+import {getUserInfo} from "@/services/auth.services";
 
 const Navbar = () => {
-  const AuthButton = dynamic(
-    () => import("@/components/ui/AuthButton/AuthButton"),
-    { ssr: false }
-  );
+    const AuthButton = dynamic(
+        () => import("@/components/ui/AuthButton/AuthButton"),
+        {ssr: false}
+    );
 
-  return (
-    <Container>
-      <Stack
-        py={2}
-        direction={"row"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-      >
-        <Typography component={Link} href="/" variant="h4" fontWeight={600}>
-          <Box component={"span"} color={"primary.main"}>
-            H
-          </Box>
-          ealth{" "}
-          <Box component={"span"} color={"primary.main"}>
-            C
-          </Box>
-          are
-        </Typography>
-        <Stack direction={"row"} gap={4} justifyContent={"space-between"}>
-          <Typography component={Link} href="/">
-            Consultation
-          </Typography>
-          <Typography component={Link} href="/">
-            Health Plans
-          </Typography>
-          <Typography component={Link} href="/">
-            Medicine
-          </Typography>
-          <Typography component={Link} href="/">
-            Diagnostics
-          </Typography>
-          <Typography component={Link} href="/">
-            NGOs
-          </Typography>
-        </Stack>
-        <AuthButton />
-      </Stack>
-    </Container>
-  );
+    const userInfo = getUserInfo();
+
+    return (
+        <Container>
+            <Stack
+                py={2}
+                direction={"row"}
+                justifyContent={"space-between"}
+                alignItems={"center"}
+            >
+                <Typography component={Link} href="/" variant="h4" fontWeight={600}>
+                    <Box component={"span"} color={"primary.main"}>
+                        H
+                    </Box>
+                    ealth{" "}
+                    <Box component={"span"} color={"primary.main"}>
+                        C
+                    </Box>
+                    are
+                </Typography>
+                <Stack direction={"row"} gap={4} justifyContent={"space-between"}>
+                    <Typography component={Link} href="/">
+                        Consultation
+                    </Typography>
+                    <Typography component={Link} href="/">
+                        Health Plans
+                    </Typography>
+                    <Typography component={Link} href="/">
+                        Medicine
+                    </Typography>
+                    <Typography component={Link} href="/">
+                        Diagnostics
+                    </Typography>
+                    <Typography component={Link} href="/">
+                        NGOs
+                    </Typography>
+                    {
+                        (userInfo && userInfo?.id) && <Typography component={Link} href="/">
+                            Dashboard
+                        </Typography>
+                    }
+                </Stack>
+                <AuthButton/>
+            </Stack>
+        </Container>
+    );
 };
 
 export default Navbar;
