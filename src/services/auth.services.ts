e0f49d36a5e4c5242601f920bf0ceafea5ea1a2f
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-06-14 18:38:16 +0600

    fix auth service getUserInfo

--- src/services/auth.services.ts
+++ src/services/auth.services.ts
@@ -1,49 +1,51 @@
 import {
-  getFromLocalStoreage,
-  removeFromLocalStorage,
-  setToLocalStorage,
+    getFromLocalStoreage,
+    removeFromLocalStorage,
+    setToLocalStorage,
 } from "@/utils/local-storage";
-import { authKey } from "@/constants/authKey";
-import { decodedToken } from "@/utils/jwt";
+import {authKey} from "@/constants/authKey";
+import {decodedToken} from "@/utils/jwt";
 import {axiosInstance} from "@/helpers/axios/axiosInstance";
 
 export const storeUserInfo = (token: string) => {
-  return setToLocalStorage(authKey, token);
+    return setToLocalStorage(authKey, token);
 };
 
 export const getUserInfo = () => {
-  const authToken = getFromLocalStoreage(authKey);
+    const authToken = getFromLocalStoreage(authKey);
 
-  if (authToken) {
-    const decodedData: any = decodedToken(authToken);
+    if (authToken) {
+        const decodedData: any = decodedToken(authToken);
 
-    return {
-      ...decodedData,
-      role: decodedData?.role.toLowerCase(),
-    };
-  }
+        return {
+            ...decodedData,
+            role: decodedData?.role.toLowerCase(),
+        };
+    } else {
+        return "";
+    }
 };
 
 export const removeUserInfo = () => {
-  return removeFromLocalStorage(authKey);
+    return removeFromLocalStorage(authKey);
 };
 
 export const isLoggedIn = () => {
-  const authToken = getFromLocalStoreage(authKey);
-  if (authToken) {
-    return true;
-  }
+    const authToken = getFromLocalStoreage(authKey);
+    if (authToken) {
+        return true;
+    }
 
-  return false;
+    return false;
 };
 
 export const getNewAccessToken = async () => {
-  return await axiosInstance({
-    url: `${process.env.NEXT_PUBLIC_SERVICE_URL}/auth/refresh-token`,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    withCredentials: true
-  })
+    return await axiosInstance({
+        url: `${process.env.NEXT_PUBLIC_SERVICE_URL}/auth/refresh-token`,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        withCredentials: true
+    })
 }
